0d03486d699eb4c8fc45c0c81bf6c6a2d060f52f
Ducklas <ng.jingrong.douglas@gmail.com>
2026-03-24 13:20:38 +0800

    hbh

---
+++ src/Item.java
@@ -0,0 +1,15 @@
+public class Item {
+    int quantity;
+    String name;
+    int test;
+    int yup;
+    int sajfbasfkjafs;
+    int dha;
+    int aasd;
+
+    Item(int q, String n){
+        quantity = q;
+        name = n;
+        
+    }
+}
